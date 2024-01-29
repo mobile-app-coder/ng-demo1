2ab3797fe2a2397bcd7194fa5fd7d78a5719baea
mobile-app-coder <you@example.com>
2024-01-29 16:26:52 +0500

    first

---
+++ android/app/src/main/kotlin/com/nextgen/ng_demo1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nextgen.ng_demo1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Next gen demo"),
+    );
+  }
+}
